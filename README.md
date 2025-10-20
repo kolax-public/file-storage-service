7e570e45b1233345ac84bdfc11a1c105421e8bca
kolax <devuser@ukr.net>
2025-10-20 12:25:49 +0300

    some  docs updates

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # File Storage Service
 
 ## Instructions
-1. Copy .env.example to .env and fill in the values.
+1. Copy .env.example to .env and fill correct values.
 2. Run `npm install`
 3. Run `docker-compose up -d` to start the services.
 4. Run `npm run migration:run` to run migrations.
 5. Access Swagger at http://localhost:3000/api
+6. Auth Google for local at http://localhost:3000/auth/google to get access_token for security Swagger endpoints.
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { ApiOperation, ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -8,12 +9,17 @@ export class AuthController {
 
   @Get('google')
   @UseGuards(AuthGuard('google'))
+  @ApiOperation({ 
+    summary: 'Initiate Google OAuth login', 
+    description: 'This endpoint initiates the Google OAuth flow. It should be accessed directly in a browser to trigger the redirect to Google for authentication.' 
+  })
   async googleAuth(@Req() req) {
     // redirect to Google
   }
 
   @Get('google/callback')
   @UseGuards(AuthGuard('google'))
+  @ApiExcludeEndpoint()
   async googleAuthRedirect(@Req() req) {
     const jwt = await this.authService.login(req.user);
     return { access_token: jwt.access_token }; 
